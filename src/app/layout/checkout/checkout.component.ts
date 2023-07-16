7b1a55be7da4d8a6228dda6070a97fea06ef7859
kritish-narula <knarula2003@gmail.com>
2023-07-17 00:48:28 +0530

    porject3

--- src/app/layout/checkout/checkout.component.ts
+++ src/app/layout/checkout/checkout.component.ts
@@ -1,27 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgxSpinner, NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent {
-
+export class CheckoutComponent implements OnInit{
+  ngOnInit(): void {
+    
+  }
   checkoutform={
     fname:'',
     lname:''
   }
-  constructor(private router:Router,private spinner:NgxSpinnerService){}
+  constructor(private router:Router,private spinner:NgxSpinnerService,private toastr:ToastrService){}
   submit(){
     // console.log(this.loginform.value.email)
     // console.log(this.loginform.value.password)
     this.spinner.show()
-    if(this.checkoutform.fname == "kritish" && this.checkoutform.lname == "narula")
+    if(this.checkoutform.fname == "" && this.checkoutform.lname == "")
     {
 
       setTimeout(() => {
         this.spinner.hide()
+        this.toastr.success("Order has been placed Successfully")
+        
       }, 4000);
     }
     else{
